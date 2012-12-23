c03e6502e17bab8356db3d5be37a1a2905d6a354
Lew <Lgordon2@gmail.com>
2012-12-23 07:24:39 -0500

    Lint cleanup.

--- src/com/app/wordservant/FlashcardScriptureReviewFragment.java
+++ src/com/app/wordservant/FlashcardScriptureReviewFragment.java
@@ -19,7 +19,6 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RelativeLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.ViewSwitcher;
 
--- src/com/app/wordservant/LandingScreen.java
+++ src/com/app/wordservant/LandingScreen.java
@@ -25,8 +25,6 @@ import android.view.Surface;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ImageView.ScaleType;
