f33f95d8e7d99318d255acd088f0686464e3b817
Lew <Lgordon2@gmail.com>
2012-12-24 00:03:09 -0500

    Fix on deleting a scripture.

--- src/com/app/wordservant/DeleteScriptureDialogFragment.java
+++ src/com/app/wordservant/DeleteScriptureDialogFragment.java
@@ -1,13 +1,15 @@
 package com.app.wordservant;
 
-import android.app.Activity;
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
+
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 
@@ -22,41 +24,39 @@ public class DeleteScriptureDialogFragment extends DialogFragment {
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
 					// Delete scripture code here...
-					SQLiteDatabase db = new WordServantDbHelper(getActivity(), WordServantContract.DATABASE_NAME, null, WordServantDbHelper.DATABASE_VERSION).getWritableDatabase();
-					String whereClause = WordServantContract.ScriptureEntry._ID+"="+getArguments().getString("_id");
-					db.delete(WordServantContract.ScriptureEntry.TABLE_NAME, whereClause, null);
+					getActivity().getContentResolver().delete(Uri.withAppendedPath(WordServantContract.ScriptureEntry.CONTENT_ID_URI_BASE,getArguments().getString("_id")), 
+							null, null);
 					String [] columns = {
 							WordServantContract.ScriptureEntry.COLUMN_NAME_NEXT_REVIEW_DATE,
 							WordServantContract.ScriptureEntry.COLUMN_NAME_SCHEDULE
 					};
-					Cursor cursor = db.query(WordServantContract.ScriptureEntry.TABLE_NAME, columns, 
+					Cursor cursor = getActivity().getContentResolver().query(WordServantContract.ScriptureEntry.CONTENT_URI, columns, 
 							WordServantContract.ScriptureEntry.COLUMN_NAME_SCHEDULE+"='daily' AND "+
-							WordServantContract.ScriptureEntry.COLUMN_NAME_NEXT_REVIEW_DATE+"=date('now','localtime')", null, null, null, null);
+							WordServantContract.ScriptureEntry.COLUMN_NAME_NEXT_REVIEW_DATE+"=date('now','localtime')", null, null);
 					if(cursor.getCount()==0){
 						String [] updateColumns = {
 							WordServantContract.ScriptureEntry._ID,
 							WordServantContract.ScriptureEntry.COLUMN_NAME_SCHEDULE
 						};
-						cursor = db.query(
-								WordServantContract.ScriptureEntry.TABLE_NAME, 
+						cursor = getActivity().getContentResolver().query(
+								WordServantContract.ScriptureEntry.CONTENT_URI, 
 								updateColumns, 
 								WordServantContract.ScriptureEntry.COLUMN_NAME_SCHEDULE+"='daily'", 
-								null, null, null, null);
+								null, null);
 						
 						ContentValues updateValues = new ContentValues();
 						Integer currentId;
+						SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd"); 
+						GregorianCalendar calendar = new GregorianCalendar();
 						for(int i=0;i<cursor.getCount();i++){
 							cursor.moveToPosition(i);
+							calendar.add(GregorianCalendar.WEEK_OF_YEAR, i==0?0:1);
 							currentId = cursor.getInt(0);
-							updateValues.put(WordServantContract.ScriptureEntry.COLUMN_NAME_NEXT_REVIEW_DATE, "date('now','localtime', '+"+i*7+" days')");
-							db.execSQL("update "+WordServantContract.ScriptureEntry.TABLE_NAME+
-									" set "+WordServantContract.ScriptureEntry.COLUMN_NAME_NEXT_REVIEW_DATE+"=date('now','localtime', '+"+i*7+" days') "+
-									" where "+WordServantContract.ScriptureEntry.COLUMN_NAME_SCHEDULE+"='daily' and "+
-									WordServantContract.ScriptureEntry._ID+"="+currentId);
+							updateValues.put(WordServantContract.ScriptureEntry.COLUMN_NAME_NEXT_REVIEW_DATE, dateFormat.format(calendar.getTime()));
+							getActivity().getContentResolver().update(Uri.withAppendedPath(WordServantContract.ScriptureEntry.CONTENT_ID_URI_BASE,String.valueOf(currentId)), 
+									updateValues, WordServantContract.ScriptureEntry.COLUMN_NAME_SCHEDULE+"='daily'", null);
 						}
 					}
-					getActivity().getContentResolver().notifyChange(WordServantContract.ScriptureEntry.CONTENT_URI, null);
-					db.close();
 				}
 				
 			})
